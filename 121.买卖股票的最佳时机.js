3d9b1370371e3bd4673761f421a9a93a98d0607e
shadowpww <68841840+shadowpww@users.noreply.github.com>
2022-11-16 21:04:47 +0800

    feat: 每日一题

--- 121.买卖股票的最佳时机.js
+++ 121.买卖股票的最佳时机.js
@@ -41,16 +41,43 @@
 //     return max;
 // };
 
+// var maxProfit = function(prices) {
+//     if(prices.length ==0)return 0;
+//     let dp = new Array(prices.length).fill(0);
+//     dp[0] = prices[0];
+//     let max = 0;
+//     for(let i=1;i<prices.length;i++){
+//         dp[i] = Math.min(prices[i],dp[i-1]);
+//         max = Math.max(max,prices[i] - dp[i-1]);
+//     }
+//     return max;
+// };
+
 var maxProfit = function(prices) {
+    // if(prices.length ==0)return 0;
+    // // let dp = new Array(prices.length).fill(0);
+    // // dp[0] = prices[0];
+    // let dp = Array.from({length:prices.length},()=> []);
+    // let n = prices.length;
+    // dp[0][0] = 0;
+    // dp[0][1] = -prices[0];
+    // for(let i=1;i<prices.length;i++){
+    //     dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+    //     dp[i][1] = Math.max(-prices[i],dp[i-1][1]);
+    // }
+    // return dp[n-1][0];
     if(prices.length ==0)return 0;
-    let dp = new Array(prices.length).fill(0);
-    dp[0] = prices[0];
-    let max = 0;
+    let n = prices.length;
+    let dp = new Array({length:n+1},()=> []);
+    dp[0][0] = 0;
+    dp[0][1] = -prices[0];
     for(let i=1;i<prices.length;i++){
-        dp[i] = Math.min(prices[i],dp[i-1]);
-        max = Math.max(max,prices[i] - dp[i-1]);
+        console.log('dp',i,dp);
+        dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+        dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);
     }
-    return max;
+    return dp[n-1][0];
 };
+
 // @lc code=end
 
